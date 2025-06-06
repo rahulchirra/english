4775ab068bc149ebdeaeef28092fe351cca7ea0a
rahul chirra <94059294+rahulchirra@users.noreply.github.com>
2025-06-06 14:45:35 +0000

    Add PWA files: index.html, manifest.json, service worker, and icons

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = 'english-pwa-cache-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
